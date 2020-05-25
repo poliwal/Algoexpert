8554e0c148cac83f49e2dba2c2c7a12df7f1ed48
poliwal <mihirpoliwal@gmail.com>
2020-05-25 10:53:10 +0530

    Recursion

---
+++ Recursion/Interweaving Strings.py
@@ -0,0 +1,57 @@
+############################### 1 ###############################
+# O(2^(n+m)) time | O(n+m) space
+def interweavingStrings(one, two, three):
+	if len(three) != len(one) + len(two):
+		return False
+	return areInterwoven(one, two, three, 0, 0)
+
+def areInterwoven(one, two, three, i, j):
+	k = i + j
+	if k == len(three) - 1:
+		return True
+
+	if i < len(one) and one[i] == three[k]:
+		if areInterwoven(one, two, three, i + 1, j):
+			return True
+
+	if j < len(two) and two[j] == three[k]:
+		return areInterwoven(one, two, three, i, j + 1)
+
+	return False
+
+
+
+
+############################### 2 ###############################
+# O(nm) time | O(nm) space
+def interweavingStrings(one, two, three):
+	if len(three) != len(one) + len(two):
+		return False
+
+	cache = [[None for _ in range(len(two) + 1)] for _ in range(len(one) + 1)]
+	return areInterwoven(one, two, three, 0, 0, cache)
+
+def areInterwoven(one, two, three, i, j, cache):
+	if cache[i][j] is not None:
+		return cache[i][j]
+
+	k = i + j
+	if k == len(three) - 1:
+		return True
+
+	if i < len(one) and one[i] == three[k]:
+		cache[i][j] = areInterwoven(one, two, three, i + 1, j, cache)
+		if cache[i][j]:
+			return True
+
+	if j < len(two) and two[j] == three[k]:
+		cache[i][j] = areInterwoven(one, two, three, i, j + 1, cache)
+		return cache[i][j]
+
+	cache[i][j] = False
+	return False
+
+
+
+
+print(interweavingStrings('aaa','aaaf','aaafaaa'))
